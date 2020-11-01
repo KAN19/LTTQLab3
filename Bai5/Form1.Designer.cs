e2aae9d8f43ffdce6a601037cb69ef3b37e4fa24
ADMIN <ADMIN@DESKTOP-U2BCLK6>
2020-11-01 20:47:15 +0700

    Commit Bai 5 ne

--- Bai5/Form1.Designer.cs
+++ Bai5/Form1.Designer.cs
@@ -28,13 +28,252 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_SoTaiKhoan = new System.Windows.Forms.TextBox();
+            this.textBox_TenKH = new System.Windows.Forms.TextBox();
+            this.textBox_DiaChi = new System.Windows.Forms.TextBox();
+            this.textBox_SoTienTrongTK = new System.Windows.Forms.TextBox();
+            this.button_Update = new System.Windows.Forms.Button();
+            this.button_Delete = new System.Windows.Forms.Button();
+            this.button_Exit = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox_SumMoney = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(726, 57);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button_Exit);
+            this.groupBox2.Controls.Add(this.button_Delete);
+            this.groupBox2.Controls.Add(this.button_Update);
+            this.groupBox2.Controls.Add(this.textBox_SoTienTrongTK);
+            this.groupBox2.Controls.Add(this.textBox_DiaChi);
+            this.groupBox2.Controls.Add(this.textBox_TenKH);
+            this.groupBox2.Controls.Add(this.textBox_SoTaiKhoan);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(0, 63);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(726, 177);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.textBox_SumMoney);
+            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Location = new System.Drawing.Point(0, 237);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(720, 233);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(142, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(462, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "QUẢN LÍ THÔNG TIN TÀI KHOẢN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(129, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Số tài khoản";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(107, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Tên khách hàng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(91, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Địa chỉ khách hàng";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(72, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(139, 16);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Số tiền trong tài khoản";
+            // 
+            // textBox_SoTaiKhoan
+            // 
+            this.textBox_SoTaiKhoan.Location = new System.Drawing.Point(237, 24);
+            this.textBox_SoTaiKhoan.Name = "textBox_SoTaiKhoan";
+            this.textBox_SoTaiKhoan.Size = new System.Drawing.Size(351, 20);
+            this.textBox_SoTaiKhoan.TabIndex = 1;
+            // 
+            // textBox_TenKH
+            // 
+            this.textBox_TenKH.Location = new System.Drawing.Point(237, 50);
+            this.textBox_TenKH.Name = "textBox_TenKH";
+            this.textBox_TenKH.Size = new System.Drawing.Size(351, 20);
+            this.textBox_TenKH.TabIndex = 1;
+            // 
+            // textBox_DiaChi
+            // 
+            this.textBox_DiaChi.Location = new System.Drawing.Point(237, 76);
+            this.textBox_DiaChi.Name = "textBox_DiaChi";
+            this.textBox_DiaChi.Size = new System.Drawing.Size(351, 20);
+            this.textBox_DiaChi.TabIndex = 1;
+            // 
+            // textBox_SoTienTrongTK
+            // 
+            this.textBox_SoTienTrongTK.Location = new System.Drawing.Point(237, 101);
+            this.textBox_SoTienTrongTK.Name = "textBox_SoTienTrongTK";
+            this.textBox_SoTienTrongTK.Size = new System.Drawing.Size(351, 20);
+            this.textBox_SoTienTrongTK.TabIndex = 1;
+            // 
+            // button_Update
+            // 
+            this.button_Update.Location = new System.Drawing.Point(286, 136);
+            this.button_Update.Name = "button_Update";
+            this.button_Update.Size = new System.Drawing.Size(100, 32);
+            this.button_Update.TabIndex = 2;
+            this.button_Update.Text = "Thêm / Cập nhật";
+            this.button_Update.UseVisualStyleBackColor = true;
+            this.button_Update.Click += new System.EventHandler(this.button_Update_Click);
+            // 
+            // button_Delete
+            // 
+            this.button_Delete.Location = new System.Drawing.Point(412, 136);
+            this.button_Delete.Name = "button_Delete";
+            this.button_Delete.Size = new System.Drawing.Size(75, 32);
+            this.button_Delete.TabIndex = 2;
+            this.button_Delete.Text = "Xóa";
+            this.button_Delete.UseVisualStyleBackColor = true;
+            this.button_Delete.Click += new System.EventHandler(this.button_Delete_Click);
+            // 
+            // button_Exit
+            // 
+            this.button_Exit.Location = new System.Drawing.Point(513, 136);
+            this.button_Exit.Name = "button_Exit";
+            this.button_Exit.Size = new System.Drawing.Size(75, 32);
+            this.button_Exit.TabIndex = 2;
+            this.button_Exit.Text = "Thoát";
+            this.button_Exit.UseVisualStyleBackColor = true;
+            this.button_Exit.Click += new System.EventHandler(this.button_Exit_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(708, 193);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // textBox_SumMoney
+            // 
+            this.textBox_SumMoney.Enabled = false;
+            this.textBox_SumMoney.Location = new System.Drawing.Point(525, 207);
+            this.textBox_SumMoney.Name = "textBox_SumMoney";
+            this.textBox_SumMoney.Size = new System.Drawing.Size(189, 20);
+            this.textBox_SumMoney.TabIndex = 1;
+            this.textBox_SumMoney.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(455, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 16);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Tổng tiền";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(721, 471);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button_Exit;
+        private System.Windows.Forms.Button button_Delete;
+        private System.Windows.Forms.Button button_Update;
+        private System.Windows.Forms.TextBox textBox_SoTienTrongTK;
+        private System.Windows.Forms.TextBox textBox_DiaChi;
+        private System.Windows.Forms.TextBox textBox_TenKH;
+        private System.Windows.Forms.TextBox textBox_SoTaiKhoan;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_SumMoney;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Bai5/Form1.cs
+++ Bai5/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,174 @@ namespace Bai5
         public Form1()
         {
             InitializeComponent();
+        }
+
+        DataTable table = new DataTable();
+        int selectedRow = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataGridViewLoad();
+        }
+
+        private void DataGridViewLoad()
+        {
+            table.Columns.Add("STT", typeof(int));
+            table.Columns.Add("Mã tài khoản", typeof(string));
+            table.Columns.Add("Tên tài khoản", typeof(string));
+            table.Columns.Add("Địa chỉ", typeof(string));
+            table.Columns.Add("Số tiền", typeof(long));
+
+            //table.Columns["STT"].AutoIncrement = true;
+            //table.Columns["STT"].AutoIncrementSeed = 0;
+            //table.Columns["STT"].AutoIncrementStep = 1;
+
+            dataGridView1.DataSource = table;
+        }
+
+        private void button_Update_Click(object sender, EventArgs e)
+        {
+            if (!CheckNullOrInvalidTextBox())
+            {
+                if (!CheckTextBoxModify())
+                {
+                    dataGridView1_AddNewData();
+                }
+                else
+                    dataGridView1_UpdateData();
+
+                TotalMoneyCalculate(); 
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private bool CheckNullOrInvalidTextBox()
+        {
+            int a = 0;
+            if (String.IsNullOrEmpty(textBox_TenKH.Text) ||
+                String.IsNullOrEmpty(textBox_SoTaiKhoan.Text) ||
+                String.IsNullOrEmpty(textBox_DiaChi.Text) ||
+                String.IsNullOrEmpty(textBox_SoTienTrongTK.Text) ||
+                !int.TryParse(textBox_SoTienTrongTK.Text, out a))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool CheckTextBoxModify()
+        {
+            if (selectedRow != 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[selectedRow];
+                if (row.Cells[1].Value != null)
+                {
+                    if (textBox_SoTaiKhoan.Text != row.Cells[1].Value.ToString() ||
+                   textBox_TenKH.Text != row.Cells[2].Value.ToString() ||
+                   textBox_DiaChi.Text != row.Cells[3].Value.ToString() ||
+                   textBox_SoTienTrongTK.Text != row.Cells[4].Value.ToString())
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            selectedRow = e.RowIndex;
+            DataGridViewRow row = dataGridView1.Rows[selectedRow];
+
+            textBox_SoTaiKhoan.Text = row.Cells[1].Value.ToString();
+            textBox_TenKH.Text = row.Cells[2].Value.ToString();
+            textBox_DiaChi.Text = row.Cells[3].Value.ToString();
+            textBox_SoTienTrongTK.Text = row.Cells[4].Value.ToString();
+
+        }
+
+        private void dataGridView1_UpdateData()
+        {
+            DataGridViewRow row = dataGridView1.Rows[selectedRow];
+
+            row.Cells[1].Value = textBox_SoTaiKhoan.Text;
+            row.Cells[2].Value = textBox_TenKH.Text;
+            row.Cells[3].Value = textBox_DiaChi.Text;
+            row.Cells[4].Value = textBox_SoTienTrongTK.Text;
+
+            MessageBox.Show("Cập nhật thông tin thành công!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void dataGridView1_AddNewData()
+        {
+            table.Rows.Add(null, textBox_SoTaiKhoan.Text, textBox_TenKH.Text, textBox_DiaChi.Text, textBox_SoTienTrongTK.Text);
+            dataGridView1.DataSource = table;
+
+            IdentifyNumberUpdate();
+
+            MessageBox.Show("Thêm mới thông tin thành công!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private void button_Delete_Click(object sender, EventArgs e)
+        {
+
+            if (dataGridView1.CurrentCell != null)
+            {
+                selectedRow = dataGridView1.CurrentCell.RowIndex;
+
+                if (MessageBox.Show("Bạn có chắc sẽ xóa dòng này?", "Information", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    dataGridView1.Rows.RemoveAt(selectedRow);
+                    IdentifyNumberUpdate();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Không tìm thấy dòng cần xóa", "Information", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void IdentifyNumberUpdate()
+        {
+            int i = 1;
+            if (dataGridView1.Rows != null)
+            {
+                DataGridViewRowCollection rows = dataGridView1.Rows;
+                foreach (DataGridViewRow item in rows)
+                {
+                    item.Cells[0].Value = i.ToString();
+                    i++;
+                }
+            }
+        }
+
+        private void TotalMoneyCalculate()
+        {
+            long total = 0;
+            if (dataGridView1.Rows != null)
+            {
+                DataGridViewRowCollection rows = dataGridView1.Rows;
+                foreach (DataGridViewRow item in rows)
+                {
+                    if (item.Cells[4].Value != null)
+                    {
+                        total += (long)item.Cells[4].Value;
+                    }
+                }
+            }
+            textBox_SumMoney.Text = total.ToString(); 
+        }
+
+        private void button_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
